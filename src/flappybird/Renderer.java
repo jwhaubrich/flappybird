0732fe638ef9f0fb989e380a257b4a3ec1f1e663
Joe Haubrich <jwhaubrich@gmail.com>
2022-06-07 13:55:33 -0500

    first FlappyBird implementation

---
+++ src/flappybird/Renderer.java
@@ -0,0 +1,16 @@
+package flappybird;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Renderer extends JPanel {
+
+    private static final long serialVersionID = 1L;
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        FlappyBird.flappyBird.repaint(g);
+    }
+}
